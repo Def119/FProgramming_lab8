40fcb07f65b3d1e81c0cd422ef08ccf55fc3d84e
Def119 <75200612+Def119@users.noreply.github.com>
2024-08-12 23:01:07 +0530

    Update q1.scala
    edited cypher circle logic

--- q1.scala
+++ q1.scala
@@ -1,27 +1,33 @@
 @main def main()={
-    var text="ThisIsTheText";
-    println("original text: " + text)
-    println("encrypted text: " + encrypt(2,text))
-    println("decrypted : " + decrypt(2,text))
-    // println(text)
-    println("decrypt " + text + " using HO func: " + cypher(2,text,decrypt))
-    println("encrypt " + text + " using HO func: " + cypher(2,text,encrypt))
+    var text="AZThisIsTheText";
+    // println("original text: " + text)
+    // println("encrypted text: " + encrypt(2,text))
+    // println("decrypted : " + decrypt(2,"AZThisIsTheText"))
+    // // println(text)
+    text = cypher(2,"Lost",encrypt)
+    println("decrypt " + text + " using HO func: " + cypher(3,"xyzA",decrypt))
+    println("encrypt " + text + " using HO func: " + cypher(2, "xyzA",encrypt))
 }
 
 def encrypt(n:Int, text: String):String={
     var encrypted:String = "";
-    for(i<-text){
-        encrypted = encrypted + (i.toInt+n).toChar
+    
+    for(i<-text){                       // can implement with map function too!!
+        var lettera = if (i.isUpper) 'A' else 'a'
+        encrypted = encrypted + ((i.toInt + n - lettera.toInt)%26 + lettera).toChar
+        
     }
     return encrypted;
 }
 
 def decrypt(n:Int, text: String):String={
-    var encrypted:String = "";
+    var decrypted:String = "";
     for(i<-text){
-        encrypted = encrypted + (i.toInt-n).toChar
+        var lettera = if (i.isUpper) 'A' else 'a'
+        decrypted = decrypted + ((i.toInt - n - lettera.toInt + 26)%26 + lettera).toChar
+        // println(s"${(i.toInt - n - lettera.toInt + 26)%26} is letter $i and ${i.toInt - n - lettera}\n")
     }
-    return encrypted;
+    return decrypted;
 }
 
 def cypher(shift:Int, text:String, func:(Int,String)=>String):String={
